d6a312e31f427b410868e05d1445193c51dfc903
holly-ctrl <hhadley12193@gmail.com>
2019-10-17 15:48:40 -0700

    node-1-after

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const products = require('./products.json')
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+// console.log(getProduct)
+const app = express()
+
+const port = 3001
+
+
+app.get('/api/products', getProducts)
+app.get('/api/product/:id', getProduct)
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+})
+
